443fb8b9a25f78c3b32385b6f7a2184a935bc923
Junyan Wu <junwu@linkedin.com>
2016-12-06 18:24:43 +0800

    change plist file to code file :easy to fix

---
+++ No Storyboard Base Application.xctemplate/code/swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testswift
+//
+//  Created by Junyan Wu on 16/12/6.
+//  Copyright © 2016年 THU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
